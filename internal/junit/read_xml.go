ca2e236cdccb88f9aa2f87e21778472e6c21981a
Brent Hughes <brent.hughes@stackpath.com>
2023-05-12 12:51:45 -0500

    fixed issue with unicode character that can't be parsed

--- internal/junit/read_xml.go
+++ internal/junit/read_xml.go
@@ -1,19 +1,30 @@
 package junit
 
 import (
+	"bytes"
 	"encoding/xml"
+	"fmt"
 	"os"
+	"unicode"
 )
 
 func ReadXML(path string) (*XML, error) {
 	fileData, err := os.ReadFile(path)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error reading file, %v", err)
 	}
 
+	// Clean the file data to remove any unicode characters that will not print properly.
+	scrubbedFileData := bytes.Map(func(r rune) rune {
+		if unicode.IsGraphic(r) {
+			return r
+		}
+		return -1
+	}, fileData)
+
 	out := new(XML)
-	if err := xml.Unmarshal(fileData, out); err != nil {
-		return nil, err
+	if err := xml.Unmarshal(scrubbedFileData, out); err != nil {
+		return nil, fmt.Errorf("error unmarshalling xml, %v", err)
 	}
 
 	return out, nil
